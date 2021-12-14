79176c853919199628a1c07214a38e4f9c85f7b2
Jack Sleeman <jsleeman1@icloud.com>
2021-12-13 21:21:38 -0700

    added correct testing for babalon

--- final/anotherbrick2_2/test.py
+++ final/anotherbrick2_2/test.py
@@ -2,38 +2,34 @@ import unittest
 import os
 import sys
 import main as kattis
-def Test(self, testin, testout):
-        inputf = open(testin)
-        input = inputf.read()
-        actualout = ""
-        firstline = True
-        h =0
-        w =0
-        n =0
-        for line in input.splitlines():
-            if firstline:
-                firstline = False
-                liney = line.split()
-                h = int(liney[0])
-                w = int(liney[1])
-                n = int(liney[2])
-            else:
-                result = str(kattis.eval(line, h, w, n))
-                if (result != "None"): #python returns None as String?
-                    actualout += str(result)
-        expectedoutfile = open(testout)
-        expectedout = expectedoutfile.read()
-        inputf.close()
-        expectedoutfile.close()
-        self.assertEqual(expectedout,actualout)
 class TestSecrets(unittest.TestCase):
     tests = [ ["test1.txt", "test1out.txt"],
                 ["test2.txt", "test2out.txt"],
                 ["test3.txt", "test3out.txt"]
              ]
-    def test_all(self):
-        for test in self.tests:
-            Test(self, test[0],test[1])
-    
+    #2 10 10
+    #2 2 2 2 2 2 2 2 2 2
+    def test_uno(self):
+        h = 2
+        w = 10
+        n = 10
+        result = str(kattis.eval("2 2 2 2 2 2 2 2 2 2", h, w, n))
+        self.assertEqual(result, "YES")
+    #4 10 8
+    #5 3 8 1 3 4 1 2
+    def test_dos(self):
+        h = 4
+        w = 10
+        n = 8
+        result = str(kattis.eval("5 3 8 1 3 4 1 2", h, w, n))
+        self.assertEqual(result, "NO")
+    #2 100 10
+    #100 10 20 10 10 10 10 10 10 10
+    def test_tres(self):
+        h = 2
+        w = 100
+        n = 10
+        result = str(kattis.eval("100 10 20 10 10 10 10 10 10 10", h, w, n))
+        self.assertEqual(result, "YES")
 if __name__ == '__main__':
     unittest.main()
